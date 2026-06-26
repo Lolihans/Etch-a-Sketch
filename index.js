f29c6cbe636838dda23e987a168b48b3a53e0fba
Lolihans <Lolihans@tutanota.com>
2026-06-26 10:52:01 +0200

    add function addSquares

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/*loop for the number of divs 
+/*request number of boxes
+loop for the number of divs 
 1 loop per flexbox line z.B. 16x16
 add the number as div to container div
 
@@ -19,12 +20,22 @@ function checkInput(squareNumber) {
     return true
   }
   else {
-    alert(`Please enter a Number between 0 and 100`)
+    alert(`Please enter a Number between 1 and 100`)
     requestInput
   }
-
 }
 
-function(squareNumber) {
-  while { squareNumber }
+function addSquares(squareNumber) {
+  let container = document.getElementById("container");
+  let rowNumber = squareNumber;
+  for (let i = 0; i < rowNumber; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    for (let i = 0; i < squareNumber; i++) {
+      let newSquare = document.createElement("div");
+      row.appendChild(newSquare);
+    }
+  }
 }
